e086d5109b7a7efaa0fafa46bf0412bb72815e76
Susetyo Pambudi <susetyamulia@gmail.com>
2022-04-07 17:20:44 +0700

    init page

---
+++ src/components/SectionOne.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+import '../styles/SectionOne.scss'
+
+function SectionOne() {
+  return (
+    <div className='section-one'>
+      <div className='player-wrapper'>
+        <ReactPlayer 
+          className="react-player"
+          width={'100vw'}
+          height={'100vh'}
+          url='https://www.youtube.com/watch?v=E3DDRW-IASA&list=RDE3DDRW-IASA&start_radio=1&autoplay=1"' 
+          playing={true}
+          stopOnUnmount={false}
+          loop
+          onReady={(obj)=>{
+            console.log(obj)
+          }}
+          muted
+          />
+      </div>
+      <div className="flex flex-col justify-center items-center h-screen relative">
+        <div className='prose prose-2xl text-white'>
+          HELLO MY NAME SUSETYO
+        </div>
+        <div className='prose prose-2xl text-white'>
+          I'M FRONTEND DEVELOPER
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SectionOne
